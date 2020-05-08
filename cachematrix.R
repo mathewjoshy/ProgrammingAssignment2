64dc72628b0b33cd98c8da8b9780f72b3fec7830
Mathew Joshy <mathewjoshy49@gmail.com>
2020-05-08 16:45:31 +0530

    The function for the assingment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,40 @@
 ## functions do
 
 ## Write a short comment describing this function
+##The first function, makeCacheMatrix creates a special "matrix".it does the following.
+##set the value of the matrix
+##get the value of the matrix
+##set the value of the inverse matrix
+##get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<-y
+    inv <<- NULL
+  }
+  get <- function()x
+  setinvmat <- function(inverse) inv <<- inverse
+  getinvmat <- function()inv
+  list(set= set, get= get,
+       setinvmat = setinvmat,
+       getinvmat = getinvmat)
 
 }
 
 
-## Write a short comment describing this function
+##This function gets the inverse of the matrix, first it checks if a inverse is calculated,
+## else it calculates the same and sets its to the cache via the setinvmat function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinvmat()
+  if(!is.null(inv)){
+    message("getting Cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <-solve(mat,...)
+  x$setinvmat(inv)
+  inv
 }
